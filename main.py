0a54a039def06af4656062d97bc5027b1a4438b1
rippertnt <rippertnt@circul.us>
2025-07-08 07:53:23 +0900

    g1 control

--- main.py
+++ main.py
@@ -10,17 +10,72 @@ from fastapi import UploadFile
 import os
 import shutil
 from fastapi.staticfiles import StaticFiles
+import asyncio
+from go2_webrtc_driver.webrtc_driver import Go2WebRTCConnection, WebRTCConnectionMethod
+from go2_webrtc_driver.constants import RTC_TOPIC, VUI_COLOR, SPORT_CMD
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+G1_ARM = {
+  "clamp": 17, 
+  "highFive": 18, 
+  "shakeHands_1": 27,
+  "makeHeartBothHands": 20, 
+  "makeHeartSingleHands": 21,
+  "blowKiss": 12, 
+  "hug": 19,
+  "hightWave": 26, 
+  "lowWave" : 25,
+  "ultramanRay" : 24, 
+  "bothHandsUp" : 15,
+  "singleHandsUp" : 23,
+  "Refuse" : 22, 
+  "Release Arm" : 99,
+}
+
+G1_STATE = {
+  "ZeroTorque" : 0,
+  "Damp" : 1,
+  "Preparation": 4,
+  "Seating": 3,       
+  "Walk_G1": 500,
+  "Walk2_G1" : 501,
+  "Run_G1" : 801,
+  "Squat_G1" : 706,  
+  "SquatUp_G1" : 706,
+  "LieUp_G1" : 702,
+}
+
+G1_BALANCE = {
+  "Stand_G1" : 0,
+  "Step_G1" : 1 
+}
 
 UPLOAD_DIR = "uploaded_audio"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # 정적 파일 서비스 (업로드한 파일 재생 가능)
 app.mount("/files", StaticFiles(directory=UPLOAD_DIR), name="files")
+origins = [
+    "http://canvers.net",
+    "https://canvers.net",   
+    "http://www.canvers.net",
+    "https://www.canvers.net",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],#origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app = FastAPI()
-camera = 0
+camera = None
+conn = None
+state = { "charge" : 0, "temp" : 0, "voltage" : 0, "cnt_live" : 0, "cnt_object" : 0 }
+
 
 def gen_frames():
     camera = cv2.VideoCapture(6)
@@ -49,6 +104,84 @@ async def index():
         </body>
     </html>
     """
+
+@app.get("/connect")
+async def connect():
+  global conn
+  #global audio_hub
+  conn =  Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, ip="192.168.123.161") #Go2WebRTCConnection(WebRTCConnectionMethod.LocalAP) #Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, ip="192.168.0.101")
+  await conn.connect()
+  def lowstate_callback(message):
+    #print(message)
+    msg = message['data']      
+    state["charge"] = msg['bms_state']['soc']
+    state["temp"] = msg['temperature_ntc1']
+    state["voltage"] = msg['power_v']
+
+  conn.datachannel.pub_sub.subscribe(RTC_TOPIC['LOW_STATE'], lowstate_callback)
+
+  return { "result" : True, "data" : True }     
+
+@app.get("/arm")
+async def arm(cmd = "clamp"):
+  global conn
+  global G1_ARM
+
+  await conn.datachannel.pub_sub.publish_request_new(
+    "rt/api/arm/request", {
+        "api_id": 7106,
+        "parameter" : { "data" : G1_ARM[cmd] }
+    }
+  )
+
+  return { "result" : True, "data" : True }      
+
+@app.get("/walk")
+async def walk(lx = 0, ly = 0, rx = 0, ry = 0):
+  print("walking",f"L : {lx} {ly} | R : {rx} {ry}")
+  global conn
+
+  conn.datachannel.pub_sub.publish_without_callback(
+     "rt/wirelesscontroller", {
+        "lx": float(lx), "ly": float(ly), "rx": float(rx), "ry": float(ry) 
+     }
+  )
+  
+  return { "result" : True, "data" : True }     
+
+@app.get("/stateG1")
+async def stateG1(cmd="Walk_G1"):
+  global conn
+  global G1_STATE
+
+  await conn.datachannel.pub_sub.publish_request_new(
+    "rt/api/sport/request", {
+        "api_id": 7101,
+        "parameter" : { "data" : G1_STATE[cmd] }
+    }
+  )
+
+  return { "result" : True, "data" : True }      
+
+@app.get("/balanceG1")
+async def balanceG1(cmd="Stand_G1"):
+  global conn
+  global G1_BALANCE
+
+  await conn.datachannel.pub_sub.publish_request_new(
+    "rt/api/sport/request", {
+        "api_id": 7102,
+        "parameter" : { "data" : G1_BALANCE[cmd] }
+    }
+  )
+
+  return { "result" : True, "data" : True }    
+
+@app.get("/heartbeat")
+async def heartbeat():
+  global state
+  print(state)
+  return { "result" : True, "data" : state }   
 
 @app.get("/video")
 def video():
