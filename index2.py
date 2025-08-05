5e59ab89a9877c793bf92ff97ce4f25df3ce0885
rippertnt <rippertnt@circul.us>
2025-08-05 11:51:13 +0900

    pdate

--- index2.py
+++ index2.py
@@ -2,4 +2,4 @@
 import uvicorn
 
 if __name__ == '__main__':    
-    uvicorn.run("main:app",host="0.0.0.0",port=59511,reload=False)
+    uvicorn.run("main2:app",host="0.0.0.0",port=59511,reload=False)
